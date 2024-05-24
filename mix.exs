ee199f3bfe538d3fa1a250070989485190bd4b39
Eddy de Boer <eddy147@gmail.com>
2024-05-24 14:28:34 +0200

    Logger not working: kafka_ex enabled

--- mix.exs
+++ mix.exs
@@ -15,20 +15,9 @@ defmodule Bla.MixProject do
   def application do
     [
         extra_applications: [
-          # :amqp,
-          # :ecto,
-          # :elixometer,
-          # :exometer,
-          # :gen_state_machine,
-          # :honeybadger,
-          # :httpoison,
-          # :jason,
-          # :kafka_ex,
+          :kafka_ex,
           :lager,
           :logger,
-          :postgrex,
-          :recase,
-          :poolboy
         ],
         mod: {Bla.Application, []}
     ]
@@ -37,28 +26,8 @@ defmodule Bla.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:amqp, "~> 3.3"},
-      {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
-      # {:ecto, "~> 3.10"},
-      # {:ecto_sql, "~> 3.10"},
-      # {:edown, "~> 0.9", override: true},
-      # {:elixometer, "~> 1.5"},
-      # {:gen_state_machine, "~> 3.0"},
-      # {:honeybadger, "~> 0.21"},
-      # {:httpoison, "~> 2.2"},
-      # {:jason, "~> 1.4"},
-      # {:kafka_ex, "~> 0.13"},
+      {:kafka_ex, "~> 0.13"},
       {:lager, "~> 3.9", override: true},
-      {:parse_trans, "~> 3.4", override: true},
-      {:poolboy, "~> 1.5"},
-      {:postgrex, "~> 0.17"},
-      {:recase, "~> 0.7"},
-      {:recode, "~> 0.6", only: :dev},
-      {:recon, "~> 2.5"},
-      {:setup, github: "uwiger/setup", override: true},
-      {:exometer, github: "PSPDFKit-labs/exometer", override: true},
-      {:exometer_core, github: "PSPDFKit-labs/exometer_core", override: true},
-      {:protox, "~> 1.6"}
     ]
   end
 end
